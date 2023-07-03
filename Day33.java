820cee855664b28a85379321ca63a57c4a928576
Pandey Subash <pandeysubash404@gmail.com>
2023-07-03 21:40:25 +0545

    Search in Rotated Sorted Array

---
+++ Day33.java
@@ -0,0 +1,43 @@
+/* Search in Rotated Sorted Array */
+public class Day33 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                // Left half is sorted in ascending order
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // Right half is sorted in ascending order
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1; // Target not found
+    }
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        Day33 solution = new Day33();
+        int result = solution.search(nums, target);
+
+        System.out.println("Index of target: " + result);
+    }
+
+}
